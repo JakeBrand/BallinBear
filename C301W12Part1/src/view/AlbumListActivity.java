c4199e5c00b3b3bb13dd5d0c471524b952740945
JakeBrand <jabrand@ualberta.ca>
2012-03-09 14:43:43 -0700

    Testing Commit

--- C301W12Part1/src/view/AlbumListActivity.java
+++ C301W12Part1/src/view/AlbumListActivity.java
@@ -7,7 +7,6 @@ import model.Album;
 import model.Item;
 import model.Photo;
 
-
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -19,7 +18,6 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
-
 
 import java.io.File;
 
@@ -35,87 +33,110 @@ import android.provider.MediaStore;
 import android.view.View;
 import android.view.View.OnClickListener;
 
-
-
 //#################################### This activity uses the main layout and is used to display the albums
-public class AlbumListActivity extends Activity {
-  ArrayList<Item> items;
-  ArrayList<Album> albs;
-    
-    
-    
-    // TODO AlbumListActivity: setup android:albumlist with current data, items have album name, sub items could have album length, the most current photo could be as a thumbnail, last updated?
-   
+public class AlbumListActivity extends Activity
+{
+
+    ArrayList<Item>  items;
+    ArrayList<Album> albs;
+
+    // TODO AlbumListActivity: setup android:albumlist with current data, items
+    // have album name, sub items could have album length, the most current
+    // photo could be as a thumbnail, last updated?
+
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        
+    public void onCreate(Bundle savedInstanceState)
+    {
+
         super.onCreate(savedInstanceState);
-      setContentView(R.layout.main);
-      
-      
+        setContentView(R.layout.main);
+
+        Button button = (Button) findViewById(R.id.NewPhotoButton);
+        OnClickListener listener = new OnClickListener()
+        {
+
+            @Override
+            public void onClick(View v)
+            {
+
+                takeNewPhoto();
+
+            }
+
+        };
+        button.setOnClickListener(listener);
     }
-    
-    private ArrayList<Item> toItem(ArrayList<Album> gal){
+
+    private ArrayList<Item> toItem(ArrayList<Album> gal)
+    {
+
         ArrayList<Item> items = new ArrayList<Item>();
-        for(int i = 0; i < gal.size(); i++){
-            items.add(new Item(gal.get(i), gal.get(i).getAlbumName(), "" + gal.get(i).getPhotos().size()));
-            
+        for (int i = 0; i < gal.size(); i++)
+        {
+            items.add(new Item(gal.get(i), gal.get(i).getAlbumName(), ""
+                    + gal.get(i).getPhotos().size()));
+
         }
         return items;
-        
+
     }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-//    private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST = 100;
-//    protected void takeAPhoto()
-//    {
-//        // Create intent instance to be used to take photo
-//        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-//        
-//        // Create Folder in SD Card                                              // folder name = tmp
-//        String folder = Environment.getExternalStorageDirectory().getAbsolutePath() + "/tmp";
-//        
-//        
-//        // give file the folder (wtf)?
-//        File folderF = new File(folder);
-//        
-//        if(!folderF.exists()){ // if folder doesnt exist
-//            folderF.mkdir();                    // make it bloody exist
-//        }
-//        
-//        // make file path                                       // name                         // type
-//        String imageFilePath = folder + "/" + String.valueOf(System.currentTimeMillis()) + ".jpg";
-//        
-//        
-//        File imageFile = new File(imageFilePath);
-//        
-//        // Create URI
-//        imageUri = Uri.fromFile(imageFile);
-//        
-//        
-//        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
-//        
-//        // start intent to take picture
-//        startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST);    
-//    }
-//    
-//    
-//    @Override                 // if requestCodeis 100 we are good, result code is to see if a picture was actually taken
-//    protected void onActivityResult(int requestCode, int resultCode,Intent intent ){
-//        if(requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST){
-//            if(resultCode == RESULT_OK){
-//                Intent intent2 = new Intent(getParent(), PhotoEditActivity.class);
-//                // set button to look like photo just taken
-//                intent2.putExtra("imagePath",imageUri.getPath()); // How do you pass around albums
-//                startActivity(intent2);
-//            }
-//        }
-//    }
+
+    private void takeNewPhoto()
+    {
+
+        Intent intent = new Intent();
+        // Other stuff for storage
+    //    startActivityForResult(intent, this);
+    }
+
+    // private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST = 100;
+    // protected void takeAPhoto()
+    // {
+    // // Create intent instance to be used to take photo
+    // Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+    //
+    // // Create Folder in SD Card // folder name = tmp
+    // String folder =
+    // Environment.getExternalStorageDirectory().getAbsolutePath() + "/tmp";
+    //
+    //
+    // // give file the folder (wtf)?
+    // File folderF = new File(folder);
+    //
+    // if(!folderF.exists()){ // if folder doesnt exist
+    // folderF.mkdir(); // make it bloody exist
+    // }
+    //
+    // // make file path // name // type
+    // String imageFilePath = folder + "/" +
+    // String.valueOf(System.currentTimeMillis()) + ".jpg";
+    //
+    //
+    // File imageFile = new File(imageFilePath);
+    //
+    // // Create URI
+    // imageUri = Uri.fromFile(imageFile);
+    //
+    //
+    // intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+    //
+    // // start intent to take picture
+    // startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST);
+    // }
+    //
+    //
+    // @Override // if requestCodeis 100 we are good, result code is to see if a
+    // picture was actually taken
+    // protected void onActivityResult(int requestCode, int resultCode,Intent
+    // intent ){
+    // if(requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST){
+    // if(resultCode == RESULT_OK){
+    // Intent intent2 = new Intent(getParent(), PhotoEditActivity.class);
+    // // set button to look like photo just taken
+    // intent2.putExtra("imagePath",imageUri.getPath()); // How do you pass
+    // around albums
+    // startActivity(intent2);
+    // }
+    // }
+    // }
 }
