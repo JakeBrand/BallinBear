90a52c380176647f4d461c8d835106a5587bed5d
JakeBrand <jabrand@ualberta.ca>
2012-03-12 14:19:43 -0600

    Working on Spinner and Albums

--- C301W12Part1/src/view/EditPhotoActivity.java
+++ C301W12Part1/src/view/EditPhotoActivity.java
@@ -41,39 +41,44 @@ import android.widget.TextView;
 
 import android.widget.Spinner;
 
-
-// TODO EditPhotoActivity: set the imageview to have the photo
-//TODO EditPhotoActivity: set the change Album to have the list of all albums plus an option to Create New Album (set the current album as the default)
-//TODO EditPhotoActivity: Update comments if there are any
 // TODO EditPhotoActivity: Set the PhotoDateLabel to have the current photos date
-
-
 
 public class EditPhotoActivity extends Activity
 {
-    
-    private Bitmap BMPphoto;
-    String[] albumNames;
 
-    Spinner albumNameSpinner;
-    
-  int albumArrayIndex;
-    int photoIndex;
-    /** Called when the activity is first created. */
+    private Bitmap BMPphoto;
+    String[]       albumNames;
+    Spinner        albumNameSpinner;
+    int            albumArrayIndex;
+    int            photoIndex;
+
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
+
         Bundle bundle = getIntent().getExtras();
-        
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.editphotoview);
         Log.e("ON CREATE IN EDIT PHOTO", "");
-       //TODO: instantiate the controller from bundle
-        // controller = (Controller) savedInstanceState.get("controller");
-       //  albumArrayIndex = (int) savedInstanceState.get("albumArrayIndex");
-        // photoIndex = (int) savedInstanceState.get("photoIndex");
-        
-        albumArrayIndex = -1;
+
+        if (bundle.containsKey("albumArrayIndex"))
+        {
+            albumArrayIndex = (int) bundle.getInt("albumArrayIndex");
+        } else
+        {
+            Log.d("EditPhotoActivity:", "albumArrayIndex was not in the bundle");
+            albumArrayIndex = -1;
+        }
+        if (bundle.containsKey("photoIndex"))
+        {
+            photoIndex = (int) bundle.getInt("photoIndex");
+        } else
+        {
+            Log.d("EditPhotoActivity:", "photoIndex was not in the bundle");
+            photoIndex = -1;
+        }
+
         ImageButton imageButton = (ImageButton) findViewById(R.id.generated_pic);
         OnClickListener generateListener = new OnClickListener()
         {
@@ -88,17 +93,20 @@ public class EditPhotoActivity extends Activity
 
         };
         imageButton.setOnClickListener(generateListener);
-        
+
         Button newAlbumButton = (Button) findViewById(R.id.newAlbumButton);
-        OnClickListener newAlbumListener = new OnClickListener(){
+        OnClickListener newAlbumListener = new OnClickListener()
+        {
+
             @Override
-            public void onClick(View v){
-                inflatePopup(); 
+            public void onClick(View v)
+            {
+
+                inflatePopup();
             }
         };
         newAlbumButton.setOnClickListener(newAlbumListener);
-        
-        
+
         Button acceptButton = (Button) findViewById(R.id.PhotoViewSave);
         OnClickListener caputreListener = new OnClickListener()
         {
@@ -106,13 +114,14 @@ public class EditPhotoActivity extends Activity
             @Override
             public void onClick(View v)
             {
-                acceptBogoPic();             
+
+                acceptBogoPic();
 
             }
 
         };
         acceptButton.setOnClickListener(caputreListener);
-        
+
         Button backButton = (Button) findViewById(R.id.PhotoViewCancel);
         OnClickListener backListener = new OnClickListener()
         {
@@ -127,70 +136,114 @@ public class EditPhotoActivity extends Activity
 
         };
         backButton.setOnClickListener(backListener);
-        
+
         Button PhotoDelete = (Button) findViewById(R.id.PhotoDelete);
-        OnClickListener PhotoDeleteListener = new OnClickListener(){
+        OnClickListener PhotoDeleteListener = new OnClickListener()
+        {
 
             @Override
             public void onClick(View v)
             {
-               
-               //TODO EditPhotoActivity: delete the Photo from the Current Album, if there are still photos in this album stay in its gallery view
-                                                                // else go to AlbumListView
+
+                // TODO EditPhotoActivity: delete the Photo from the Current
+                // Album, if there are still photos in this album stay in its
+                // gallery view
+                // else go to AlbumListView
             }
-            
+
         };
         PhotoDelete.setOnClickListener(PhotoDeleteListener);
-        
-       
-        //TODO EditPhotoActivity: get the id of the current album albumNameSpinner.get, if (new album) selected go to edit album to create new album, if existing album selected
-        //                                                                 add this photo to the album and go to the album view after finishing the current activity
+
+        // TODO EditPhotoActivity: get the id of the current album
+        // albumNameSpinner.get, if (new album) selected go to edit album to
+        // create new album, if existing album selected
+        // add this photo to the album and go to the album view after finishing
+        // the current activity
         albumNameSpinner = (Spinner) findViewById(R.id.albumNameSpinner);
         albumNames = Controller.getAlbumNames();
+        // TODO: REMOVE ME *************//
+        albumNames = new String[] {};   //
+                                        //
+        // TODO: DONE ******************//
+        ArrayAdapter<String> spinnerAdapter;
+        if (albumNames.length != 0)
+        {
+            spinnerAdapter = new ArrayAdapter<String>(
+                    EditPhotoActivity.this,
+                    android.R.layout.simple_spinner_item, albumNames);
 
-        ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<String>(EditPhotoActivity.this, android.R.layout.simple_spinner_item, albumNames);
-        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
-        albumNameSpinner.setAdapter(spinnerAdapter);
+        } else {
+            spinnerAdapter = new ArrayAdapter<String>(
+                    EditPhotoActivity.this,
+                    android.R.layout.simple_spinner_item, new String[]{"No Albums"});
+        }
+            spinnerAdapter
+                    .setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+            albumNameSpinner.setAdapter(spinnerAdapter);
         
+
     }
 
-    private void inflatePopup(){
+    private void inflatePopup()
+    {
 
-         AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setTitle("New Album");
+        alert.setMessage("Enter the name of the new Album");
 
-         alert.setTitle("New Album");
-         alert.setMessage("Enter the name of the new Album");  
+        // Set an EditText view to get user input
+        final EditText input = new EditText(this);
+        alert.setView(input);
 
-         // Set an EditText view to get user input
-         final EditText input = new EditText(this);
-         alert.setView(input);
+        alert.setPositiveButton("Ok", new DialogInterface.OnClickListener()
+        {
 
-         alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-         public void onClick(DialogInterface dialog, int whichButton) {
-           Editable value = input.getText();
-           Log.e("value", value.toString());
+            public void onClick(DialogInterface dialog, int whichButton)
+            {
 
-             Log.e("InflatePopup","Button Clicked");
-             String newAlbumName = value.toString();
-             albumNames[albumNames.length-1] = newAlbumName;
-             ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<String>(EditPhotoActivity.this, android.R.layout.simple_spinner_item, albumNames);
-             spinnerAdapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
-             albumNameSpinner.setAdapter(spinnerAdapter);
-             albumNameSpinner.setSelection(albumNames.length -1, false);
-           }
-         });
+                Editable value = input.getText();
+                Log.e("value", value.toString());
 
-         alert.setNegativeButton("Cancel", new
- DialogInterface.OnClickListener() {
-           public void onClick(DialogInterface dialog, int whichButton) {
-             // Canceled.
-           }
-         });
+                Log.e("InflatePopup", "Button Clicked");
+                String newAlbumName = value.toString();
+                // TODO: REMOVE ME ***************
+                albumNames = new String[] {};
 
-         alert.show();
-        
+                // TODO: DONE *******************
+                String[] temp = new String[albumNames.length + 1];
+                temp[0] = newAlbumName;
+                for (int i = 0; i < albumNames.length; i++)
+                {
+                    temp[i] = albumNames[i + 1];
+                }
+                albumNames = temp;
+                temp = null;
+                // albumNames[albumNames.length - 1] = newAlbumName; //TODO: no
+                // overwrite
+                ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<String>(
+                        EditPhotoActivity.this,
+                        android.R.layout.simple_spinner_item, albumNames);
+                spinnerAdapter
+                        .setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+                albumNameSpinner.setAdapter(spinnerAdapter);
+                albumNameSpinner.setSelection(albumNames.length - 1, false);
+            }
+        });
+
+        alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener()
+        {
+
+            public void onClick(DialogInterface dialog, int whichButton)
+            {
+
+                // Canceled.
+            }
+        });
+
+        alert.show();
+
     }
-    
+
     protected void cancelBogoPic()
     {
 
@@ -206,6 +259,7 @@ public class EditPhotoActivity extends Activity
 
     protected void acceptBogoPic()
     {
+
         // get intent that starts the activity
         Intent intent = getIntent();
         if (intent == null)
@@ -214,69 +268,54 @@ public class EditPhotoActivity extends Activity
             return;
         }
 
-          //  File intentFile = getPicturePath(intent);
-            Uri imageUri = getImageUri(intent);
-            Bundle b = new Bundle();
-            //saveBMP(intentFile, BMPphoto);
+        // File intentFile = getPicturePath(intent);
+        Uri imageUri = getImageUri(intent);
+        Bundle b = new Bundle();
+        // saveBMP(intentFile, BMPphoto);
 
-            // if clicked, get the edittext
-            EditText commentET = (EditText) findViewById(R.id.commentEditText);
-            String comment = commentET.getText().toString();        // get the string inside
+        // if clicked, get the edittext
+        EditText commentET = (EditText) findViewById(R.id.commentEditText);
+        String comment = commentET.getText().toString(); // get the string
+                                                         // inside
 
-            if(albumArrayIndex == -1){
-                Controller.addAlbum(albumNameSpinner.getSelectedItem().toString(), imageUri, comment);
-                b.putInt("albumArrayIndex", 0);
-            }
-            else{
-                Controller.addPhoto(albumArrayIndex, imageUri, comment);
-                b.putInt("albumArrayIndex", albumArrayIndex);
-                
-            }
-            //intent.putExtra("BMPphoto", BMPphoto);
-            
-            
+        if (albumArrayIndex == -1)
+        {
+            Controller.addAlbum(albumNameSpinner.getSelectedItem().toString(),
+                    imageUri, comment);
+            b.putInt("albumArrayIndex", 0);
+        } else
+        {
+            Controller.addPhoto(albumArrayIndex, imageUri, comment);
+            b.putInt("albumArrayIndex", albumArrayIndex);
 
-            
+        }
 
-            intent.putExtras(b);
+        intent.putExtras(b);
 
-            
-            
-            setResult(RESULT_OK, intent);
-        
+        setResult(RESULT_OK, intent);
+
         finish();
 
     }
-// TODO: Save the BMP or create a Photo and save the Photo?
+
+    // TODO: Save the BMP or create a Photo and save the Photo?
     /*
-    private void saveBMP(File intentFile, Bitmap ourBMP2)
-    {
-
-        OutputStream stream;
-        try
-        {
-            stream = new FileOutputStream(intentFile);
-            BMPphoto.compress(Bitmap.CompressFormat.JPEG, 75, stream);
-            stream.close();
-        } catch (FileNotFoundException e)
-        {
-            e.printStackTrace();
-        } catch (IOException e)
-        {
-            e.printStackTrace();
-        }
-
-    }
-
-    private File getPicturePath(Intent intent)
-    {
-
-        Uri uri = (Uri) intent.getExtras().get(MediaStore.EXTRA_OUTPUT);
-        File file = new File(uri.getPath());
-
-        return file;
-    }
-    */
+     * private void saveBMP(File intentFile, Bitmap ourBMP2) {
+     * 
+     * OutputStream stream; try { stream = new FileOutputStream(intentFile);
+     * BMPphoto.compress(Bitmap.CompressFormat.JPEG, 75, stream);
+     * stream.close(); } catch (FileNotFoundException e) { e.printStackTrace();
+     * } catch (IOException e) { e.printStackTrace(); }
+     * 
+     * }
+     * 
+     * private File getPicturePath(Intent intent) {
+     * 
+     * Uri uri = (Uri) intent.getExtras().get(MediaStore.EXTRA_OUTPUT); File
+     * file = new File(uri.getPath());
+     * 
+     * return file; }
+     */
     private Uri getImageUri(Intent intent)
     {
 
@@ -284,22 +323,20 @@ public class EditPhotoActivity extends Activity
         return uri;
     }
 
-
-
     protected void setBogoPic()
     {
 
         ImageButton button = (ImageButton) findViewById(R.id.generated_pic);
-        BMPphoto = BogoPicGen.generateBitmap(350,350);
+        BMPphoto = BogoPicGen.generateBitmap(350, 350);
         button.setImageBitmap(BMPphoto);
     }
 
-    public void onPause(){
+    public void onPause()
+    {
+
         super.onPause();
         Controller.saveObject(this);
-        
+
     }
-    
-    
-    
+
 }
