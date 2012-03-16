1de0a9d226b9947f17bcff77db91f55ba0a16e8d
JakeBrand <jabrand@ualberta.ca>
2012-03-16 08:21:47 -0600

    Pushing Testing Suite

---
+++ C301W12Part1/src/control/PhotoController.java
@@ -0,0 +1,18 @@
+package control;
+
+import java.io.File;
+
+import view.EditPhotoActivity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+
+
+public class PhotoController
+{
+
+
+    
+}
---
+++ TestSuite/src/ca/ualberta/ca/c301/test/EditPhotoActivityTest.java
@@ -0,0 +1,23 @@
+package ca.ualberta.ca.c301.test;
+
+import view.EditPhotoActivity;
+import android.test.ActivityInstrumentationTestCase2;
+
+
+public class EditPhotoActivityTest extends ActivityInstrumentationTestCase2<EditPhotoActivity>
+{
+
+    public EditPhotoActivityTest(String pkg,
+            Class<EditPhotoActivity> activityClass)
+    {
+
+        super("view", EditPhotoActivity.class);
+    }
+    
+    // No need for @test annotation because we extend the testCase (everything is a test)
+    // note: assertEquals is in italics because it is static. (but we don't have to do a static import here0
+    public void testTest(){
+        assertEquals(8,16/2);
+    }
+
+}
