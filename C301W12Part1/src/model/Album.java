01719f3a66b91197efe9a6762ecce34914890e49
JakeBrand <jabrand@ualberta.ca>
2012-03-28 01:58:15 -0600

    Javadoc Updated

--- C301W12Part1/src/model/Album.java
+++ C301W12Part1/src/model/Album.java
@@ -1,137 +1,157 @@
 package model;
+
 import java.io.Serializable;
 import java.util.ArrayList;
 
-
-
-
-public class Album implements Serializable{
-	/**
-	 *  @author J-Tesseract
-	 */
-    
+/**
+ * @author J-Tesseract
+ */
+//TODO: Make better Javadoc. EXPLAIN ALL CLASSES AND PARAMETERS/RETURN
+public class Album implements Serializable
+{
 
     private static final long serialVersionUID = 2277251080502459333L;
-    
+
     /**
      * album name and the list of photos are fields
      */
-    private  String albumName;
-    private ArrayList<Photo> photos;
-    
-    
+    private String            albumName;
+    private ArrayList<Photo>  photos;
+
     /**
      * Contructor
      * 
      * @param albumName
+     *            The name of the album being constructed
      */
-    public Album(String albumName) {
+    public Album(String albumName)
+    {
+
         this.albumName = albumName;
         this.photos = new ArrayList<Photo>();
     }
-    
-    
+
     /**
-     * deleteAll
-     * deletes all Photos in album
+     * deleteAll 
+     * 
+     * Deletes all Photos in this album
      */
-    public void deleteAll(){
-        for(int i=size()-1;i>=0;i--){
+    public void deleteAll()
+    {
+
+        for (int i = size() - 1; i >= 0; i--)
+        {
             deletePhoto(i);
         }
     }
-    
-    
+
     /**
-     * size 
-     * @return size of photos
+     * size
+     * 
+     * Determine the size of the array holding this Album's Photos
+     * 
+     * @return Size of array of Photos
      */
-    public int size(){
+    public int size()
+    {
+
         return photos.size();
     }
-    
-    
+
     /**
      * getAlbumName
      * 
-     * @return albumName
+     * @return albumName The current name of the Album
      */
-    public  String getAlbumName() {
+    public String getAlbumName()
+    {
+
         return albumName;
     }
-    
+
     /**
      * setAlbumName
      * 
-     * @param albumName
+     * @param albumName The new name of the Album
      */
-    public void setAlbumName(String albumName)  {
+    public void setAlbumName(String albumName)
+    {
+
         this.albumName = albumName;
     }
-    
-    
+
     /**
      * getPhotos
      * 
-     * @return photos
+     * @return photos The ArrayList<Photo> in this album
      */
-    public ArrayList<Photo> getPhotos() {
+    public ArrayList<Photo> getPhotos()
+    {
+
         return photos;
     }
-    
+
     /**
      * setPhotos
      * 
      * @param photos
      */
-    public void setPhotos(ArrayList<Photo> photos) {
+    public void setPhotos(ArrayList<Photo> photos)
+    {
+
         this.photos = photos;
     }
-    
+
     /**
      * getPhoto
      * 
      * @param photoIndex
      * @return Photo at photoIndex in photos
      */
-    public Photo getPhoto(int photoIndex){
+    public Photo getPhoto(int photoIndex)
+    {
+
         return photos.get(photoIndex);
     }
-    
-    
+
     /**
      * addPhoto
      * 
      * @param p
      */
-    public void addPhoto(Photo p){
+    public void addPhoto(Photo p)
+    {
+
         photos.add(p);
     }
-    
+
     /**
      * deletePhoto
      * 
      * @param photoIndex
      */
-    public void deletePhoto(int photoIndex){
+    public void deletePhoto(int photoIndex)
+    {
+
         photos.remove(photoIndex);
     }
-    
-    
+
     /**
      * updatePhoto
      * 
      * @param photoIndex
      * @param photo
      */
-    public void updatePhoto(int photoIndex, Photo photo){
+    public void updatePhoto(int photoIndex, Photo photo)
+    {
+
         photos.set(photoIndex, photo);
-//        if(photo.getPicture().equals(photos.get(photoIndex).getPicture()))
-//            photos.set(photoIndex, photo);
-//        else{
-//        photos.remove(photoIndex);
-//        photos.add(photo);
-//        }
+        // if(photo.getPicture().equals(photos.get(photoIndex).getPicture()))
+        // photos.set(photoIndex, photo);
+        // else{
+        // photos.remove(photoIndex);
+        // photos.add(photo);
+        // }
     }
 
 }
--- C301W12Part1/src/model/Photo.java
+++ C301W12Part1/src/model/Photo.java
@@ -10,91 +10,103 @@ import control.Controller;
 import android.net.Uri;
 import android.util.Log;
 
+/**
+ * @author J-Tesseract
+ */
+
 public class Photo implements Serializable
 {
 
-	/**
-	 *  @author J-Tesseract
-     * fields
+    /**
+     * Fields of photo
      */
     private static final long serialVersionUID = -2561097008925968276L;
     private Date              pTimeStamp;
     private String            comment;
     private Uri               imageUri;
     private ArrayList<String> tags;
-    
-    
 
     /**
-     * hasTag 
+     * hasTag
      * 
-     * given a String s, determines if the Photo has been 'auto-tagged' with the String
+     * given a String tag, determines if the Photo has been 'auto-tagged' with tag
      * 
-     * @param s
-     * @return
+     * @param tag The tag being searched for
+     * @return True if the photo has the provided tag
      */
-    public boolean hasTag(String s){
+    public boolean hasTag(String tag)
+    {
+
         int i = 0;
         boolean hasT = false;
-        while(i < tags.size() && !hasT){
-            hasT = (tags.get(i).equals(s));
+        while (i < tags.size() && !hasT)
+        {
+            hasT = (tags.get(i).equals(tag));
             i++;
         }
         return hasT;
-   
+
     }
 
-    
     /**
      * constructor
      * 
      * @param comm
+     *            The comment atssociated with the Photo
      * @param imageU
+     *            The Uri the Bitmap is stored at
      */
-    public Photo(String comm, Uri imageU){
+    public Photo(String comm, Uri imageU)
+    {
+
         this.pTimeStamp = new Date(System.currentTimeMillis());
         this.comment = comm;
         this.imageUri = imageU;
         this.tags = generateTags(comm);
-        
+
     }
 
     /**
      * generateTags
      * 
-     * generates an ArrayList of Strings using the comment and common words to 'auto-tag' the photo.
+     * generates an ArrayList of Strings using the comment and common words to
+     * 'auto-tag' the photo.
      * 
      * @param comm
-     * @return
+     *            The comment used to generate tags
+     * @return The ArrayList<String> of all tags
      */
-    private ArrayList<String> generateTags(String comm){
+    private ArrayList<String> generateTags(String comm)
+    {
+
         StringTokenizer token = new StringTokenizer(comm);
         String str;
         int count = 0;
         ArrayList<String> tags = new ArrayList<String>();
-        while (token.hasMoreTokens()){
-        count++;
-        
-        str = token.nextToken();
-        if(Controller.inTags(str)){
-            Log.e(null,str);
-        tags.add(str);
+        while (token.hasMoreTokens())
+        {
+            count++;
+
+            str = token.nextToken();
+            if (Controller.inTags(str))
+            {
+                Log.e(null, str);
+                tags.add(str);
+            }
+
         }
-        
-        //Log.e(null,str);
-        }
-        
-        
+
         return tags;
     }
-
 
     /**
      * getpTimeStamp
      * 
-     * @return pTimeStamp
+     * @return pTimeStamp The current timestamp of the Photo
      */
-    public Date getpTimeStamp()  {
+    public Date getpTimeStamp()
+    {
+
         return pTimeStamp;
     }
 
@@ -102,18 +114,22 @@ public class Photo implements Serializable
      * setpTimeStamp
      * 
      * @param pTimeStamp
+     *            The new timestamp for the Photo
      */
-    public void setpTimeStamp(Date pTimeStamp) {
+    public void setpTimeStamp(Date pTimeStamp)
+    {
+
         this.pTimeStamp = pTimeStamp;
     }
 
-    
     /**
      * getComent
      * 
-     * @return comment
+     * @return comment The current comment of the Photo
      */
-    public String getComment(){
+    public String getComment()
+    {
+
         return this.comment;
     }
 
@@ -121,33 +137,36 @@ public class Photo implements Serializable
      * setComment
      * 
      * @param com
+     *            The new comment of the Photo
      */
-    public void setComment(String com){
+    public void setComment(String com)
+    {
+
         this.comment = com;
         this.tags = generateTags(com);
     }
-
 
     /**
      * setPicture
      * 
      * @param imageU
+     *            The Uri the Bitmap is stored at
      */
-    public void setPicture(Uri imageU){
+    public void setPicture(Uri imageU)
+    {
+
         this.imageUri = imageU;
     }
 
-    
     /**
      * getPicture
      * 
-     * @return imageUri
+     * @return imageUri The Uri the Bitmap is stored at
      */
-    public Uri getPicture(){
+    public Uri getPicture()
+    {
 
         return this.imageUri;
     }
-
-
 
 }
